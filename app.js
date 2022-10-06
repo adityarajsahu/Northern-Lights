54b19bd0d8bce67b35d355b56581d73bac17d61e
unknown <adityaraj2019.sahu@gmail.com>
2022-10-06 10:14:43 +0530

    new route added - add new campground

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const exp = require('constants');
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -20,21 +21,33 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-})
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
 
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
-})
+});
 
 let server_port = 3000 || process.env.YOUR_PORT || process.env.PORT;
 app.listen(server_port, () => {
     console.log(`Listening on port ${server_port}`);
-}) 
+});
