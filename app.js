934d38ebb742104742b16254731d7d8b9f9bf0f6
unknown <adityaraj2019.sahu@gmail.com>
2022-10-21 23:14:37 +0530

    ejs-mate engine added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 const Campground = require('./models/campground');
 
@@ -19,6 +20,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
